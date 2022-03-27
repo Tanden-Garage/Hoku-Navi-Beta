03c424038f18635e74b0ae9a8b2c95fe18883106
Kosuke2000 <youuchiharu@gmail.com>
2022-03-27 21:39:28 +0900

    feat: ClubCardの雛形の作成

---
+++ src/components/model/Club/ClubCard/ClubCard.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import { VFC } from "react"
+
+import { ClubProps } from "@/types/Club"
+
+import { Anchor } from "@/components/ui/Anchor"
+
+export const ClubCard: VFC<ClubProps> = ({ club }) => {
+  const { id, name } = club
+  // https://hubcnavi.net/storage/orgs/eb16dc0b783842079f83/large.png
+  const imgSrc = `https://hubcnavi.net/storage/orgs/${id}/large.png`
+
+  return (
+    <Anchor className="no-underline" href={`/${id}`}>
+      <div className="w-80 border-2 shadow-xl card bg-base-100">
+        <figure className="m-0 rounded-t-xl">
+          <Image
+            className="p-0 m-0 rounded-t-xl"
+            src={imgSrc}
+            width={400}
+            height={300}
+            alt={name}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="m-0 card-title">{name}</h2>
+          <p className="m-0">
+            If a dog chews shoes whose shoes does he choose?
+          </p>
+        </div>
+      </div>
+    </Anchor>
+  )
+}
