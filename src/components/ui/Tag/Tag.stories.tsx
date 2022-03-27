b1311c419518a508147665176079af0a957bd436
Kosuke2000 <youuchiharu@gmail.com>
2022-03-27 14:44:58 +0900

    feat: TagListのStorybookを作成

--- src/components/ui/Tag/Tag.stories.tsx
+++ src/components/ui/Tag/Tag.stories.tsx
@@ -1,6 +1,8 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react"
 import React from "react"
 
+import { TagList } from "./Tag"
+
 import { Tag } from "./index"
 
 export default {
@@ -8,12 +10,19 @@ export default {
   component: Tag,
 } as ComponentMeta<typeof Tag>
 
-const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />
+const SingleTemplate: ComponentStory<typeof Tag> = (args) => <Tag {...args} />
 
 const soccerTag = {
   label: "サッカー",
   path: "soccer",
 }
 
-export const Default = Template.bind({})
-Default.args = { tag: soccerTag }
+export const Single = SingleTemplate.bind({})
+Single.args = { tag: soccerTag }
+
+const ListTemplate: ComponentStory<typeof TagList> = (args) => (
+  <TagList {...args} />
+)
+
+export const List = ListTemplate.bind({})
+List.args = { tags: [soccerTag, soccerTag, soccerTag, soccerTag] }
