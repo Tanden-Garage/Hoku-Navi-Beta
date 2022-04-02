84e4339e5301b2f9e50c730e2de83ad046770124
Hajime Nakagawa <navyblue0914@icloud.com>
2022-04-02 10:18:35 +0900

    feat: Drawerの雛形を実装

---
+++ src/components/ui/Drawer/Drawer.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react"
+import React from "react"
+
+import { Drawer } from "./index"
+
+export default {
+  title: "UI/Drawer",
+  component: Drawer,
+} as ComponentMeta<typeof Drawer>;
+
+const Template: ComponentStory<typeof Drawer> = (args) => (
+  <Drawer {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {}
