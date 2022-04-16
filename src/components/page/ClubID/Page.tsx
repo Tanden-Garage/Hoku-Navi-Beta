af6eb9b6a19e277539ba4bc292578c3eea5b59e4
Kosuke2000 <youuchiharu@gmail.com>
2022-04-16 16:10:42 +0900

    fix: Headタグをやめて、MySEOタグに変更

--- src/components/page/ClubID/Page.tsx
+++ src/components/page/ClubID/Page.tsx
@@ -1,5 +1,4 @@
-import Head from "next/head"
-
+import { MySEO } from "@/components/functional/SEO"
 import { medSoccer } from "@/mocks/Club"
 
 import { ClubIdPageView } from "./View"
@@ -9,11 +8,11 @@ import type { NextPageWithLayout } from "next"
 export const ClubIdPage: NextPageWithLayout = () => {
   return (
     <>
-      <Head>
-        <title>club</title>
-        <meta name="description" content="Write page description here." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <MySEO
+        title={`${medSoccer.name} | 北大部活サークルnavi β`}
+        description={`${medSoccer.name}の詳細ページ`}
+        path={`/${medSoccer.id}`}
+      />
 
       <ClubIdPageView club={medSoccer} />
     </>
