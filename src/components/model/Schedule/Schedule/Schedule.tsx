ba2f1741d3ce4710c274eae85bdb3feaeedc833d
Kosuke2000 <youuchiharu@gmail.com>
2022-04-16 15:53:01 +0900

    fix: 見た目とコードの構造上 borderをScheduleItemあてるのは直感に反するのでScheduleへ変更

--- src/components/model/Schedule/Schedule/Schedule.tsx
+++ src/components/model/Schedule/Schedule/Schedule.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const ScheduleItem: VFC<Props> = ({ month, event }) => {
   return (
-    <div className="relative pl-5 h-36 border-l-2 last:border-base-100 border-secondary">
+    <div className="relative pl-5 h-36">
       <div className="inline-flex absolute  top-0 -left-1.5 w-2.5 h-2.5 rounded-full  border-2 border-primary  bg-primary"></div>
       <div className="absolute -top-2 left-5 prose-sm">
         <h3 className="text-md">{month}</h3>
@@ -25,7 +25,7 @@ export const ScheduleItem: VFC<Props> = ({ month, event }) => {
 
 export const Schedule: VFC<ScheduleProps> = ({ schedule }) => {
   return (
-    <div className="">
+    <div className="border-l-2  border-secondary">
       {Object.entries(schedule).map(
         ([month, event]) =>
           event !== "" && (
