643ada8308e83f0e0fbc4e95f9c6b7967568f852
Kosuke2000 <youuchiharu@gmail.com>
2022-04-17 13:11:41 +0900

    refactor: divをよりセマンティックにした

--- src/components/model/Schedule/Schedule/Schedule.tsx
+++ src/components/model/Schedule/Schedule/Schedule.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const ScheduleItem: VFC<Props> = ({ month, event }) => {
   return (
-    <div className="relative pl-5 h-36">
+    <section className="relative pl-5 h-36">
       <div className="inline-flex absolute  top-0 -left-1.5 w-2.5 h-2.5 rounded-full  border-2 border-primary  bg-primary" />
       <div className="absolute -top-2 left-5 prose-sm">
         <h3 className="text-md">{month}</h3>
@@ -17,17 +17,17 @@ export const ScheduleItem: VFC<Props> = ({ month, event }) => {
       <div className="flex absolute top-8 right-0 left-5 items-center p-8 rounded-xl shadow-2xl bg-base-100">
         <p>{event}</p>
       </div>
-    </div>
+    </section>
   )
 }
 
 export const Schedule: VFC<ScheduleProps> = ({ schedule }) => {
   return (
-    <div className="border-l-2  border-secondary">
+    <article className="border-l-2  border-secondary">
       {Object.entries(schedule).map(
         ([month, event]) =>
           event !== "" && <ScheduleItem month={month} event={event} />
       )}
-    </div>
+    </article>
   )
 }
