43e80aacde0ab777247d5526cbdf2f775a1acc3e
Kosuke2000 <youuchiharu@gmail.com>
2022-04-30 16:46:39 +0900

    feat: 索引ページの雛形を作成

--- src/components/page/Sakuin/View.tsx
+++ src/components/page/Sakuin/View.tsx
@@ -1,36 +1,74 @@
-import type { Sakuin } from "@/utils/sakuin"
+import { useState, VFC } from "react"
+import { AiOutlineInfoCircle } from "react-icons/ai"
+
+import type { ClubItem, Sakuin } from "@/utils/sakuin"
 
 import { Anchor } from "@/components/ui/Anchor"
 import { Spacer } from "@/components/ui/Spacer"
 
-import type { VFC } from "react"
+type Key = keyof Sakuin
 
 interface SakuinPageViewProps {
   items: Sakuin
 }
 
 export const SakuinPageView: VFC<SakuinPageViewProps> = ({ items }) => {
+  const [activeKey, setActiveKey] = useState<Key>("あ行")
+
+  const handleClick = (key: Key) => setActiveKey(key)
+
+  const current: ClubItem[] = items[activeKey]
+
   return (
-    <main className="flex flex-col   p-4 mx-auto max-w-3xl prose">
-      <Spacer size={16} />
+    <main className="flex flex-col p-4 mx-auto max-w-3xl h-screen prose">
+      <Spacer size={4} />
 
-      <h1>団体さくいん</h1>
+      <h1>
+        団体さくいん
+        <span
+          className="align-text-bottom tooltip tooltip-info tooltip-bottom md:tooltip-right"
+          data-tip="ひらがなアイコンをクリックしてみて！"
+        >
+          <AiOutlineInfoCircle size={28} className="text-info" />
+        </span>
+      </h1>
 
-      {Object.entries(items).map(([行, items]) => (
-        <>
-          <h2 className="py-1 px-2 bg-info">{行}</h2>
-          <ul className="flex flex-wrap w-full">
-            {items.length === 0 && <span>該当なし😢</span>}
-            {items.map((item) => (
-              <li key={item.name} className="w-1/2">
-                <Anchor href={item.path} className="hover:underline">
-                  {item.name}
-                </Anchor>
-              </li>
-            ))}
-          </ul>
-        </>
-      ))}
+      <div className="flex flex-wrap gap-4 justify-center mt-0.5 w-full lg:mt-0">
+        {(Object.keys(items) as Key[]).map((key) => {
+          const avater = key.slice(0, 1)
+
+          // TODO: active時のデザイン決め
+          const styleIsActive = key === activeKey ? "bg-info" : "bg-base-100"
+          const iconIsActive = key === activeKey ? "🥸" : avater
+
+          return (
+            <div
+              className={`shrink-0 border border-primary w-10 h-10 rounded-full ${styleIsActive}`}
+              onClick={() => handleClick(key)}
+              key={key}
+            >
+              <p className="m-0 text-3xl text-center align-middle">
+                {iconIsActive}
+              </p>
+            </div>
+          )
+        })}
+      </div>
+
+      <Spacer size={8} />
+
+      <h2 className="py-1 px-2 m-0 bg-info">{activeKey}</h2>
+
+      <ul className="flex flex-wrap w-full">
+        {current.length === 0 && <span>該当なし😢</span>}
+        {current.map((club) => (
+          <li key={club.name} className="w-1/2">
+            <Anchor href={club.path} className="hover:underline">
+              {club.name}
+            </Anchor>
+          </li>
+        ))}
+      </ul>
 
       <Spacer size={20} />
     </main>
