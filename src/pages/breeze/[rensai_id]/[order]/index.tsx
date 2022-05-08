f1db61a244134998fe50c2ec6c3806f292d86780
Kosuke2000 <youuchiharu@gmail.com>
2022-05-08 13:02:03 +0900

    feat: articleページの作成

---
+++ src/pages/breeze/[rensai_id]/[order]/index.tsx
@@ -0,0 +1,7 @@
+import ArticlePage from "@/components/page/Article"
+
+import { Layout } from "@/components/layout"
+
+ArticlePage.getLayout = (page) => <Layout>{page}</Layout>
+
+export default ArticlePage
